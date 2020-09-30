599f18d26d91b7ac015de6a1b3cac29331c3f3ef
Andrea <dev@sysmodelers.com>
2020-09-30 16:27:09 -0400

    initial commit

---
+++ src/components/TrackForm.js
@@ -0,0 +1,41 @@
+import React, { useContext } from "react";
+import { StyleSheet } from "react-native";
+import { Input, Button } from "react-native-elements";
+import Spacer from "./Spacer";
+import { Context as LocationContext } from "../context/LocationContext";
+import useSaveTrack from "../hooks/useSaveTracks";
+
+const TrackForm = () => {
+  const {
+    state: { name, recording, locations },
+    startRecording,
+    stopRecording,
+    changeName,
+  } = useContext(LocationContext);
+  const [saveTracks] = useSaveTrack();
+
+  return (
+    <>
+      <Spacer>
+        <Input
+          value={name}
+          placeholder="Enter name of the track"
+          onChangeText={changeName}
+        />
+      </Spacer>
+      {recording ? (
+        <Button title="Stop " onPress={stopRecording} />
+      ) : (
+        <Button title="Start Recording" onPress={startRecording} />
+      )}
+      <Spacer />
+      {!recording && locations.length ? (
+        <Button title="Save Recording" onPress={saveTracks} />
+      ) : null}
+    </>
+  );
+};
+
+export default TrackForm;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,43 @@
+import React, { useContext, useEffect } from "react";
+import { View, StyleSheet } from "react-native";
+import { NavigationEvents } from "react-navigation";
+import NavLink from "../components/NavLink";
+import AuthForm from "../components/AuthForm";
+import { Context as AuthContext } from "../context/AuthContext";
+
+const SignupScreen = () => {
+  const { state, signup, clearErrorMessage } = useContext(AuthContext);
+
+  return (
+    <View style={styles.container}>
+      <NavigationEvents onWillBlur={clearErrorMessage} />
+      <AuthForm
+        headerText="Sign Up for Tracker"
+        errorMessage={state.errorMessage}
+        submitButtonText="Sign Up"
+        onSubmit={({ email, password }) => signup({ email, password })}
+      />
+
+      <NavLink
+        routeName="Signin"
+        text="Already have an account? Sign in instead"
+      />
+    </View>
+  );
+};
+
+SignupScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 200,
+  },
+});
+
+export default SignupScreen;
